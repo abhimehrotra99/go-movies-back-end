43c516796e5c516214703ff906f53cacc0545c36
abhimehrotra99 <abhimehrotra99@gmail.com>
2023-05-04 06:22:13 +0530

    implementing db interface

--- internal/repository/dbrepo/postgres_dbrepo.go
+++ internal/repository/dbrepo/postgres_dbrepo.go
@@ -1,1 +1,18 @@
 package dbrepo
+
+import (
+	"backend/internal/models"
+	"database/sql"
+	"time"
+)
+
+type PostgresDBRepo struct {
+	DB *sql.DB
+}
+
+const dbTimeout = time.Second * 3
+
+func (m *PostgresDBRepo) AllMovies() ([]*models.Movie, error) {
+	var movies []*models.Movie
+	return movies, nil
+}
